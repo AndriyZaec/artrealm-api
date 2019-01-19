89a13f446750d5a5b6c5815a0433477b8ec4e919
Andrew Zaiets <andrew.zaec@gmail.com>
2019-01-19 18:48:11 +0200

    init :]

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  App
+//
+//  Created by Andrew Zaiets on 1/19/19.
+//
+
+import Foundation
+import Vapor
+import Fluent
+import FluentSQLite
+import Authentication
+
+struct User: Content {
+    
+}
